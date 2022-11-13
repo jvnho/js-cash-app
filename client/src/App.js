d627d0640aa717887c953a913e89f72d8150b5f5
jvnho <nicolassangsiri@gmail.com>
2022-11-13 16:35:37 +0100

    Remove useless function, added html class and id

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,9 @@ class ArticleButton extends React.Component{
   render(){
     const article = this.props.obj;
     const available = (article.quantity > 0);
-    let button;
-    if (available) {
-      button = <button onClick={() => 
-        this.props.onArticleClicked(new Article(this.props.obj.name, this.props.obj.price, 1))
-        }>
-        {this.props.obj.name} {formatPrice(this.props.obj.price)}</button>
-    } else {
-      button = <button disabled={true}>{this.props.obj.name} {formatPrice(this.props.obj.price)}</button>
-    }
-    return button;
+    return (<button className="article-add-btn" disabled={!available} onClick={() => 
+        this.props.onArticleClicked(new Article(this.props.obj.name, this.props.obj.price, 1))}>
+        {this.props.obj.name} {formatPrice(this.props.obj.price)}</button>)
   }
 }
 
@@ -26,9 +19,9 @@ class ArticleCart extends React.Component{
     const price = article.getPrice();
     const quantity = article.getQuantity();
     return (
-      <div>
-        <p>{name} {price}€ {quantity} {formatPrice(price*quantity)}</p>
-        <button onClick={() => this.props.onArticleClicked(article)}>
+      <div className="cart-element-container">
+        <p className="article-cart-detail" >{name} {formatPrice(price)}€ {quantity} {formatPrice(price*quantity)}</p>
+        <button className="article-remove-btn" onClick={() => this.props.onArticleClicked(article)}>
           -
         </button>
       </div>
@@ -40,10 +33,10 @@ class Catalog extends React.Component{
 
   render(){
     return(
-      <ul>
+      <ul id="catalog-list-container">
       {
       this.props.articles.map((object, i) =>
-        <li key={i}><ArticleButton obj={object} onArticleClicked={(article) => this.props.onArticleClicked(article)}/></li>
+        <li className="catalog-list-element" key={i}><ArticleButton obj={object} onArticleClicked={(article) => this.props.onArticleClicked(article)}/></li>
       )}
       </ul>
     )
@@ -53,10 +46,10 @@ class Catalog extends React.Component{
 class Cart extends React.Component{
   render(){
     return(
-      <ul>
+      <ul id="cart-list-container">
       {
       this.props.cart.map((object, i) =>
-        <li key={i}><ArticleCart obj={object} onArticleClicked={(article) => this.props.onArticleClicked(article)}/></li>
+        <li className="cart-list-element" key={i}><ArticleCart obj={object} onArticleClicked={(article) => this.props.onArticleClicked(article)}/></li>
       )}
       </ul>
     )
@@ -90,64 +83,21 @@ class Container extends React.Component{
     }
     return -1;
   }
-
-    /**
-   * Returns a new list of articles while replacing article with a new quantity, allowing function to be pure.
-   * @param articles list of articles
-   * @param article Article to be add or remove
-   * @param add boolean indicates if it's a add or remove operation
-   */
-  updateCartState(article, add){
-    const articles = this.state.cart;
-    const idx = this.getIndex(article);
-    var quantity = 1;
-    if(idx === -1){ // No such article in the cart list
-      quantity = 1;
-    } else {
-      quantity = (add ? articles[idx].getQuantity()+1 : articles[idx].getQuantity()-1);
-    }
-    var newArticle = new Article(article.getName(), article.getPrice(), quantity);
-    var newList = []
-    for(var i = 0; i < articles.length; i++){
-      if(i !== idx){
-        newList.push(articles[i]);
-      } else {
-        newList.push(newArticle);
-      }
-    }
-    if(idx === -1){ // Article didn't exist so we add it to the end
-      newList.push(newArticle);
-    }
-    this.setState((state,props) => ({
-      cart: newList
-    }));
-  }
-
   /**
    * Handler that allows user to add an article to the cart state array 
+   * Function will either add an article if it's not already in the list or will replace it with a new object with a new quantity
    * @param article to add
    */
   addToCart(article){
     const articles = this.state.cart;
     const idx = this.getIndex(article);
-    var quantity = 1;
-    if(idx === -1){ // No such article in the cart list
-      quantity = 1;
-    } else {
-      quantity = articles[idx].getQuantity()+1 
-    }
+    var quantity = (idx === -1 ? 1 : articles[idx].getQuantity()+1);
     var newArticle = new Article(article.getName(), article.getPrice(), quantity);
     var newList = []
     for(var i = 0; i < articles.length; i++){
-      if(i !== idx){
-        newList.push(articles[i]);
-      } else {
-        newList.push(newArticle);
-      }
+      (i !== idx ? newList.push(articles[i]) : newList.push(newArticle));
     }
-    if(idx === -1){ // Article didn't exist so we add it to the end
-      newList.push(newArticle);
-    }
+    if(idx === -1) newList.push(newArticle); // Article didn't exist so we add it to the end
     this.setState((state,props) => ({
       cart: newList,
       total: state.total + parseFloat(article.getPrice()),
@@ -156,7 +106,8 @@ class Container extends React.Component{
 
   /**
    * Handler that allows user to remove article from the cart state array
-   * @param article to add
+   * Function will either remove an article if its new quantity is zero or will replace it with a new object with a new quantity
+   * @param article to remove
    */
   removeFromCart(article){
     const articles = this.state.cart;
@@ -165,11 +116,10 @@ class Container extends React.Component{
     var newArticle = new Article(article.getName(), article.getPrice(), quantity);
     var newList = []
     for(var i = 0; i < articles.length; i++){
-      if(i !== idx){
+      if(i !== idx)
         newList.push(articles[i]);
-      } else {
+      else 
         if(quantity > 0) newList.push(newArticle);
-      }
     }
     this.setState((state,props) => ({
       cart: newList,
@@ -179,19 +129,18 @@ class Container extends React.Component{
 
   render(){
     return (
-      <div id="container">
-        <p>{this.props.data.companyName}</p>
-        <p>{this.props.data.companyAddress}</p>
-        <p>{this.props.data.companyPhoneNumber}</p>
-        <p>{this.props.data.companyMail}</p>
+      <div id="app-container">
+        <p className="shop-information">{this.props.data.companyName}</p>
+        <p className="shop-information">{this.props.data.companyAddress}</p>
+        <p className="shop-information">{this.props.data.companyPhoneNumber}</p>
+        <p className="shop-information">{this.props.data.companyMail}</p>
         
-        <div id="catalog">
+        <div id="catalog-container">
           <Catalog articles={this.props.data.articles} onArticleClicked={this.addToCart}/>
         </div>
-        <div id="cart"> 
+        <div id="cart-container"> 
           <Cart cart={this.state.cart} onArticleClicked={this.removeFromCart}/>
-          <p>{formatPrice(this.state.total)}</p>
-          <button>ORDER</button>
+          <button id="order-cart-button">ORDER {formatPrice(this.state.total)}</button>
         </div>
       </div>
     );
@@ -202,22 +151,18 @@ function App(){
   const [backendData, setBackendData] = useState([{}]);
 
   useEffect(() => {
-    fetch("/datas/").then(
-      response => response.json()
-    ).then(
-      data =>{
-        setBackendData(data)
-      }
-    )
-  }, [])
+    fetch("/datas/")
+      .then(response => response.json())
+      .then(data => {setBackendData(data)})}, []);
+
+  var rendered;
+  if(typeof backendData.companyName === "undefined")
+    rendered = <p>Loading...</p>;
+  else 
+    rendered = <Container data={backendData}/>;
 
   return(
-    <div>
-      {(typeof backendData.companyName === "undefined") ? (
-        <p>Loading...</p>
-      ) :
-      <Container data={backendData}/>}
-    </div>
+    rendered
   )
 }
 
